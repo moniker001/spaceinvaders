36d13859646d055ef6f1ce932bed025da0ec2b39
moniker001 <lim.tejong@gmail.com>
2015-02-23 02:25:28 -0600

    added shooting animation and basic ui

--- SpaceInvaders.elm
+++ SpaceInvaders.elm
@@ -21,58 +21,188 @@ import Time
 areaW = 500
 areaH = 500
 
-type State = MoveL | MoveR | Idle
+type Move = MoveL | MoveR | Idle
 
-type alias Object =
-  { x:Float, y:Float, vx:Float, dir:State}
+type LaserStatus = Ready | Shooting | Collision
 
-player : Object
-player =
-  { x=0, y=-220, vx=0, dir=Idle}
+type State = Play | Pause
+
+type alias Player =
+  { x    : Float
+  , y    : Float
+  , vx   : Float
+  , vy   : Float
+  , move : Move
+  }
+
+type alias Laser =
+  { x_laser : Float
+  , y_laser : Float
+  , vx      : Float
+  , vy      : Float
+  , status  : LaserStatus
+  , fired   : Time
+  }
+
+type alias Game =
+  { state  : State
+  , player : Player
+  , laser  : Laser
+  , score  : Float
+  }
+
+initGame : Game
+initGame =
+  { state  = Pause
+  , player = 
+    { x    = 0
+    , y    = -220
+    , vx   = 0
+    , vy   = 0
+    , move = Idle
+    }
+  , laser  = 
+    { x_laser = 0
+    , y_laser = -215
+    , vx      = 0
+    , vy      = 0
+    , status  = Ready
+    , fired   = 0
+    }
+  , score  = 0
+  }
+
+type alias Input =
+  { start     : Bool
+  , pause     : Bool
+  , playerDir : Int
+  , shoot     : Bool
+  , delta     : Time
+  }
 
 -- UPDATE
 
-update : (Time, { x:Int, y:Int }) -> Object -> Object
-update (timeDelta, direction) obj =
-  obj
-    |> setVelocity direction
-    |> setDirection direction
-    |> setPosition timeDelta
+update_game : Input -> Game -> Game
+update_game input game =
+  let
+    {start, pause, playerDir, shoot, delta} = input
+    {state, player, laser, score} = game
+    newState = state
+      |> start_game start
+      |> pause_game pause
+    newPlayer = 
+      case state of
+        Pause -> player
+        Play -> 
+          player
+            |> update_vel_player   playerDir
+            |> update_move_player  playerDir
+            |> update_pos_player   delta
+    newLaser = 
+      case state of
+        Pause -> laser
+        Play -> 
+          laser
+            |> update_pos_laser    delta player
+            |> update_status_laser shoot
+  in
+    { game  |
+        state <- newState,
+        player <- newPlayer,
+        laser <- newLaser
+    }
 
-setVelocity : { x:Int, y:Int } -> Object -> Object
-setVelocity {x,y} obj =
+start_game : Bool -> State -> State
+start_game start state =
+  case state of
+    Pause -> if
+      | start     -> Play
+      | otherwise -> state
+    Play  -> state
+
+pause_game : Bool -> State -> State
+pause_game pause state =
+  case state of
+    Pause -> if
+      | pause     -> Play
+      | otherwise -> state
+    Play  -> if
+      | pause     -> Pause
+      | otherwise -> state
+
+-- UPDATE LASER
+
+update_pos_laser : Time -> Player -> Laser -> Laser
+update_pos_laser dt player laser =
+  let
+    { x_laser, y_laser, status, fired } = laser
+    { x, y } = player
+  in
+  case status of
+    Shooting -> if
+      | y_laser > 250 ->
+          { laser |
+              x_laser <- x,
+              y_laser <- -215,
+              status  <- Ready
+          }
+      | otherwise -> 
+          { laser | y_laser <- y_laser + dt * 5 }
+    Ready ->
+      { laser | 
+          x_laser <- x,
+          y_laser <- -215 
+      }
+    Collision -> { laser | y_laser <- -215 }
+
+update_status_laser : Bool -> Laser -> Laser
+update_status_laser shoot laser =
+  let {x_laser, y_laser, vx, vy, status, fired} = laser in
+  case status of
+    Ready -> if
+      | shoot -> { laser | status <- Shooting}
+      | otherwise -> laser
+    Shooting ->  laser     
+    Collision -> { laser | status <- Ready}
+
+-- UPDATE PLAYER
+
+update_vel_player : Int -> Player -> Player
+update_vel_player x player =
   let
     newVel n =
-      if x == 0 || y == 0
+      if x == 0
         then toFloat n
-        else toFloat n / sqrt 2
+        else toFloat n * 2
   in
-      { obj |
+      { player |
           vx <- newVel x
       }
 
-setDirection : { x:Int, y:Int } -> Object -> Object
-setDirection {x,y} obj =
-  { obj |
-      dir <-
+update_move_player : Int -> Player -> Player
+update_move_player x player =
+  { player |
+      move <-
         if  | x > 0 -> MoveR
             | x < 0 -> MoveL
-            | otherwise -> obj.dir
+            | otherwise -> player.move
   }
 
-
-setPosition : Time -> Object -> Object
-setPosition dt ({x,y,vx,dir} as obj) =
-  { obj |
+update_pos_player : Time -> Player -> Player
+update_pos_player dt player =
+  let {x, y, vx, vy, move} = player in
+  { player |
       x <- clamp (-areaW/2) (areaW/2) (x + dt * vx)
   }
 
-
 -- VIEW
 
-view : (Int,Int) -> Object -> Element
-view (w,h) {x,y,vx,dir} =
+view : (Int,Int) -> Game -> Element
+view (w,h) game =
   let
+    { state, player, laser, score } = game
+    { x, y, vx, move } = player
+    { x_laser, y_laser, status, fired } = laser
     w' = toFloat (w - 1)
     h' = toFloat (h - 1)
   in
@@ -80,35 +210,153 @@ view (w,h) {x,y,vx,dir} =
     <| C.collage areaW areaH
         [ C.rect w' h'
           |> C.filled black
-        , [ C.rect 40 10
-            |> C.filled red,
-            C.oval 7 15
-            |> C.filled red
-            |> C.moveY 5
-          ]
-            |> C.group
-            |> C.move (x,y)
+        , renderLaser game
+        , renderPlayer game
+        , renderHowTo game
         , "Space Invaders"
-          |> T.fromString
-          |> T.color white
-          |> T.height 40
-          |> T.centered
-          |> C.toForm
-          |> C.moveY 220
+            |> T.fromString
+            |> T.color white
+            |> T.height 40
+            |> T.centered
+            |> C.toForm
+            |> C.moveY 220
+        , userInterface game
         ]
 
+renderHowTo : Game -> C.Form
+renderHowTo game =
+  let
+    { state } = game
+    size = 30
+  in
+  case state of
+    Pause ->
+      [ "How to Play"
+          |> T.fromString
+          |> T.color white
+          |> T.height (size + 10)
+          |> T.centered
+          |> C.toForm
+          |> C.moveY (size * 2)
+      , "Move - Arrow Keys or WASD"
+          |> T.fromString
+          |> T.color white
+          |> T.height size
+          |> T.centered
+          |> C.toForm
+          |> C.moveY (size * 1)
+      , "Shoot - Space"
+          |> T.fromString
+          |> T.color white
+          |> T.height size
+          |> T.centered
+          |> C.toForm
+          |> C.moveY (size * 0)
+      , "Enter - Start Game"
+          |> T.fromString
+          |> T.color white
+          |> T.height size
+          |> T.centered
+          |> C.toForm
+          |> C.moveY (size * -1)
+      ]
+        |> C.group
+    Play -> [] |> C.group
+
+renderPlayer : Game -> C.Form
+renderPlayer game =
+  [ C.rect 40 10
+    |> C.filled red
+  , C.oval 7 15
+    |> C.filled red
+    |> C.moveY 5
+  ]
+    |> C.group
+    |> C.move (game.player.x, game.player.y)
+
+renderLaser : Game -> C.Form
+renderLaser game =
+  let
+    { state, player, laser, score } = game
+    { x_laser, y_laser, status, fired } = laser
+  in
+  case status of
+    Ready -> 
+      C.rect 1 15
+        |> C.filled black
+        |> C.move (x_laser, y_laser)
+    _ ->
+      C.rect 1 15
+        |> C.filled white
+        |> C.move (x_laser, y_laser)
+
+userInterface : Game -> C.Form
+userInterface game =
+  let
+    { state, player, laser, score } = game
+    { x, y, vx, move } = player
+    { x_laser, y_laser, status } = laser
+    size = 20
+    borderStyle = 
+      let ls = C.defaultLine in
+        { ls | color <- darkGray, width <- 5 }
+  in
+  [ C.rect 150 75
+    |> C.outlined borderStyle
+    |> C.moveY 15
+  , score
+    |> toString
+    |> T.fromString
+    |> T.append (T.fromString "Score : ")
+    |> T.color green
+    |> T.height size
+    |> T.centered
+    |> C.toForm
+    |> C.moveY (size * 2)
+  , move
+    |> toString
+    |> T.fromString
+    |> T.append (T.fromString "Move : ")
+    |> T.color green
+    |> T.height size
+    |> T.centered
+    |> C.toForm
+    |> C.moveY (size * 1)
+  , status
+    |> toString
+    |> T.fromString
+    |> T.append (T.fromString "Fire : ")
+    |> T.color green
+    |> T.height size
+    |> T.centered
+    |> C.toForm
+    |> C.moveY (size * 0)
+  ]
+    |> C.group
+    |> C.move (150, 140)
 
 -- SIGNALS
 
 main : Signal Element
 main =
-  Signal.map2 view Window.dimensions
-    (Signal.foldp update player input)
+  Signal.map2 view Window.dimensions gameState
 
-input : Signal (Time, { x:Int, y:Int })
+gameState : Signal Game
+gameState =
+  Signal.foldp update_game initGame input
+
+input : Signal Input
 input =
-  Signal.sampleOn delta
-    (Signal.map2 (,) delta Keyboard.wasd)
+  Signal.sampleOn delta playerInput
+
+playerInput : Signal Input
+playerInput =
+  Signal.map5 Input
+    Keyboard.enter
+    (Keyboard.isDown 80)
+    (.x <~ (Signal.merge Keyboard.arrows Keyboard.wasd))
+    Keyboard.space
+    delta
 
 delta : Signal Time
 delta =
