4e1c93c3cdbd169dda549fdbe70b4c87211b0a93
moniker001 <lim.tejong@gmail.com>
2015-03-04 21:10:17 -0600

    import global

--- src/Object.elm
+++ src/Object.elm
@@ -1,9 +1,10 @@
 module Object where
 
-import Vector (Vector, vec)
-import Vector as V
+import Global (..)
 import Graphics.Collage (Form)
 import Graphics.Collage as Form
+import Vector (Vector, vec)
+import Vector as V
 
 type alias Object ext =
   { ext
@@ -18,11 +19,12 @@ type alias Object ext =
 render : Object a -> Form
 render object = Form.move object.pos object.gfx
 
-stepPosition : Float -> Vector -> Object a -> Vector
-stepPosition delta gameDimensions object =
+stepPosition : Event -> Object a -> Vector
+stepPosition ((d, ks, { x, y }) as event) object =
   let (hx, hy) = V.scale (0.5) object.dim
-      (hw, hh) = V.scale (0.5) gameDimensions
+      (hw, hh) = V.scale (0.5) (vec gWidth gHeight)
+      effectiveVel = player.vel |> V.cross (vec x y) |> V.scale delta
   in
       V.bound (vec (-hw + hx) (-hh + hy))
               (vec ( hw - hx) ( hh - hy))
-              (V.add object.pos (V.scale delta object.vel))
+              (V.add object.pos effectiveVel)
